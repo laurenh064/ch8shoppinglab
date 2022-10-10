b47b55f9e4a0b1d00fe3dba51b15d4fa15eeaae3
Lauren Hartley <lahartley99@gmail.com>
2022-10-10 13:23:06 -0400

    implement output cart row and calculate total

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,32 @@
 /* define your functions here */
 
+console.log(cart);
+
+function calculateTotal(quantity, price) {
+    return quantity * price;
+}
+
+function outputCartRow(item, total) {
+    console.log(item, total);
+    const { product: { title, filename, price }, quantity } = item;
+    const row = `
+    <tr>
+    <td><img src="images/${filename}" alt="${title}"></td>
+    <td>${title}</td>
+    <td>${quantity}</td>
+    <td>${price}</td>
+    <td>${total}</td>
+    </tr>`;
+    document.write(row);
+
+}
+
+function outputTableBody() {
+    for (let item of cart) {
+        let total = calculateTotal(item.quantity, item.product.price);
+        outputCartRow(item, total);
+    }
+}
 
 
 
-
-
-
-
-        
